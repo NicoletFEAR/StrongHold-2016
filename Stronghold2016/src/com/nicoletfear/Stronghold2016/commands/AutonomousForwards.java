206ec5e76d9323f02f314d2c60776b5f3171078a
John-Campbell <12baysidejsc@gmail.com>
2016-02-06 16:24:34 -0600

    Got rid of commented out code to make merging easier

--- Stronghold2016/src/com/nicoletfear/Stronghold2016/commands/AutonomousForwards.java
+++ Stronghold2016/src/com/nicoletfear/Stronghold2016/commands/AutonomousForwards.java
@@ -5,29 +5,6 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class AutonomousForwards extends CommandGroup {
 	
     public  AutonomousForwards() {
-    	//addSequential(new Forward(5.5));
     	addSequential(new Forwards());
-        
-        //backward = new Backward(2);
-        //addSequential(new Forward(2));
-    	//if (backward != null) backward.start();
-    	
-    	
-        // Add Commands here:
-        // e.g. addSequential(new Command1());
-        //      addSequential(new Command2());
-        // these will run in order.
-
-        // To run multiple commands at the same time,
-        // use addParallel()
-        // e.g. addParallel(new Command1());
-        //      addSequential(new Command2());
-        // Command1 and Command2 will run in parallel.
-
-        // A command group will require all of the subsystems that each member
-        // would require.
-        // e.g. if Command1 requires chassis, and Command2 requires arm,
-        // a CommandGroup containing them would require both the chassis and the
-        // arm.
     }
 }
