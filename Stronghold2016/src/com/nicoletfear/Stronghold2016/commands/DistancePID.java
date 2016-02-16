ca9c2acd043ecfd249bf020032d1d9bce926ec15
noah <noah@noah-Latitude-E6410>
2016-02-16 10:07:36 -0600

    Added end method to interrupted method in DistancePID command. Also added partially redundant code to ensure that the bot stops when the command is finished.

--- Stronghold2016/src/com/nicoletfear/Stronghold2016/commands/DistancePID.java
+++ Stronghold2016/src/com/nicoletfear/Stronghold2016/commands/DistancePID.java
@@ -49,12 +49,13 @@ public class DistancePID extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
-    	
+    	Robot.driveTrain.drive(0, 0);
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	end();
     }
     
     private boolean leftIsWithinError(){
