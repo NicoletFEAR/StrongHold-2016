0e6be1a89633638792e3f1a31bc5cb8f21e69770
connor-calderon <calderonc@nicoletfear.com>
2016-02-27 16:45:18 -0600

    Added Version number display on smartdashboard

--- Stronghold2016/src/com/nicoletfear/Stronghold2016/Robot.java
+++ Stronghold2016/src/com/nicoletfear/Stronghold2016/Robot.java
@@ -123,6 +123,8 @@ public class Robot extends IterativeRobot {
 	 */
 	public void teleopPeriodic() {
 		Scheduler.getInstance().run();
+		SmartDashboard.putNumber("Arm Position", arm.positionMotor.getEncPosition());
+		SmartDashboard.putString("Version", "5.1");
 	}
 
 	/**
