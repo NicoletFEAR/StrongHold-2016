e069d2bcaebf8efa9df5103202ca0f29ed78ae78
connor-calderon <calderonc@nicoletfear.com>
2016-03-03 21:04:02 -0600

    AutoArmDown for low bar Auto
    Command to move the arm down at the beginning of the low bar Autonomous.

---
+++ Stronghold2016/src/com/nicoletfear/Stronghold2016/commands/AutoArmDown.java
@@ -0,0 +1,47 @@
+
+package com.nicoletfear.Stronghold2016.commands;
+
+import com.nicoletfear.Stronghold2016.Robot;
+import com.nicoletfear.Stronghold2016.RobotMap;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *Command that lowers arm. It stops when the user releases the
+ *lower limit switch or releases the button
+ */
+public class AutoArmDown extends Command {
+	
+    public AutoArmDown() {
+        // Use requires() here to declare subsystem dependencies
+        requires(Robot.arm);
+        setTimeout(RobotMap.armDownTimer);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	Robot.arm.armDown();
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return isTimedOut();
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	//Robot.arm.armHold();
+    	//new HoldArm().start();
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    	end();
+    }
+}
--- Stronghold2016/src/com/nicoletfear/Stronghold2016/commands/AutonomousLowBar.java
+++ Stronghold2016/src/com/nicoletfear/Stronghold2016/commands/AutonomousLowBar.java
@@ -5,6 +5,7 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class AutonomousLowBar extends CommandGroup {
 	
     public  AutonomousLowBar() {
+    	addSequential(new AutoArmDown());
     	addSequential(new LowBar());
     	//runs forwards
     }
